62f0a881967b281d5de767cd31cf7ba9ed6ce0b1
Nathan Trost <n.d.trost@gmail.com>
2024-01-16 15:28:34 -0600

    reactHookDebugger
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ReactHookDebugger
-A Development tool created with the purpose of validating dependency changes in a React hook
+A Development tool created with the purpose of validating dependency changes in React hooks.
+
+The primary purpose of this snippet is to aid in troubleshooting runaway renders, it's currently fitted for useEffect, useMemo, and useCallback; especially for those with multiple complex dependencies. As your code will cycle through these hooks, you will be able to see just how frequent it's cycling through and what dependencies are changing and triggering your hook to update.
+
+## Example
+
+### Setup initial code
+[Copy and paste snippet here](https://github.com/NathanTrost/ReactHookDebugger/blob/main/src/reactHookDebugger.ts), either in it's own file or at the top of the current file you're working on.  
+<b>Note:</b> This utility is in it's very early stages, future iterations may set it up as an import.
+
+
+### Use on your hook as such
+```
+reactHookDebugger("My useEffect",
+   // Text representations of your dependencies in the order they've been listed below,
+   // if not provided they will be displayed as "[N] **Dependency Name Unknown**"
+   [
+      "boolDependency1", 
+      "boolDependency2",
+      "boolDependency3",
+      "objectDependency"
+   ]).useEffect(() => {
+   
+      // .....logic reliant on all dependencies
+   
+
+   }, [boolDependency1, boolDependency2, boolDependency3, objectDependency]);
+```
+
+### Result
+![image](https://github.com/NathanTrost/ReactHookDebugger/assets/12831882/97552c7e-eb8e-4982-854a-b15987a31087)
+
+
---
+++ src/reactHookDebugger.ts
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  DependencyList,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+
+/**
+ * Tooling function specific for local development that fires with any useEffect, useMemo, or useCallback hook and provides performance information on that hook to the console.
+ * Insights include:
+ * - Initial values of dependencies on mount
+ * - Number of times the hook has been triggered
+ * - Before and After values of dependencies
+ *
+ * @param {string} name - Provide unique name to more easily distinguish your hook's console from any other hook you may be using this tool with.
+ * @param {string[]} dependencyNames - Provide an array of dependency/variable names which will be aligned with the dependencies provided to your hook.  If not provided,
+ *                          you will still be able to distinguish the changed dependencies that are triggering your hook by thier placement in the original
+ *                          dependency array.
+ *                          Ex:  If dependencies are listed as [left, right, top, bottom], you would set this values as ["left", "right", "top", "bottom"]
+ * @returns {useEffect | useMemo | useCallback} Chained React hook
+ */
+export const reactHookDebugger = (
+  name = "General",
+  dependencyNames: string[] = []
+) => {
+  return {
+    useEffect: (doFunc: any, dependencies: DependencyList) => {
+      useDebugHook("useEffect", dependencyNames, dependencies, name);
+      useEffect(doFunc, dependencies);
+    },
+    useMemo: (doFunc: any, dependencies: DependencyList) => {
+      useDebugHook("useMemo", dependencyNames, dependencies, name);
+      return useMemo(doFunc, dependencies);
+    },
+    useCallback: (doFunc: any, dependencies: DependencyList) => {
+      useDebugHook("useCallback", dependencyNames, dependencies, name);
+      return useCallback(doFunc, dependencies);
+    },
+  };
+};
+
+const usePrevious = (value: any, initialValue: any[]) => {
+  const ref = useRef(initialValue);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+const useDebugHook = (
+  hookName: "useEffect" | "useMemo" | "useCallback",
+  dependencyNames: string[],
+  dependencies: DependencyList,
+  name: string
+) => {
+  const [count, setCount] = useState(0);
+  const previousDeps = usePrevious(dependencies, []);
+
+  const changedDeps = dependencies.reduce(
+    (accum: any, dependency: any, index: number) => {
+      if (dependency !== previousDeps[index]) {
+        const dependencyName =
+          dependencyNames[index] || "**Dependency Name Unknown**";
+
+        const keyName = `[${index}] ${dependencyName}`;
+        return {
+          ...accum,
+          [keyName]: {
+            before: previousDeps[index],
+            after: dependency,
+          },
+        };
+      }
+
+      return accum;
+    },
+    {}
+  );
+
+  if (count === 0 && dependencyNames.length !== dependencies.length) {
+    console.warn(
+      `[debugHook_${hookName}]:${name} ->
+      Without a 1:1 list of dependencyNames to dependencies the information provided by this tool
+      may not be reliable.  It's recommended you provide the full list of dependency to aid in clarity
+      and visibility of which dependencies are triggering updates in this ${hookName}.`
+    );
+  }
+
+  useEffect(() => {
+    const datetime = new Date();
+    if (count === 0) {
+      console.groupCollapsed(
+        `[debugHook_${hookName}]:${name} -> Initial values on mount`
+      );
+      console.log(`CallCount[${count}]`, changedDeps);
+      console.log(`Timestamp: ${datetime.toString()}`);
+      console.groupEnd();
+      setCount(1);
+    } else if (Object.keys(changedDeps as {}).length) {
+      console.groupCollapsed(
+        `[debugHook_${hookName}]:${name} -> Rerender[${count}]`
+      );
+      console.log(`CallCount[${count}]`, changedDeps);
+      console.log(`Timestamp: ${datetime.toString()}`);
+      console.groupEnd();
+      setCount(count + 1);
+    }
+  });
+};
